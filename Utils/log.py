340aa7f6a04bb9de7b2e7d30468550b4e6644e17
NimVrod <hdec0759@gmail.com>
2024-08-24 16:12:01 +0200

    Added a review reminder in log.py

--- Utils/log.py
+++ Utils/log.py
@@ -5,7 +5,7 @@ import datetime
 
     
 async def send_log(embed: discord.Embed, channel:discord.TextChannel) -> None:
-    embed.set_footer(text=f"LoggerBot | Developed by: nimvrod | [Suppport Server](https://discord.gg/sJuQPRkDkq)")
+    embed.set_footer(text=f"LoggerBot | Developed by: nimvrod | If you're enjoying LoggerBot, please consider leaving a review on Top.gg! https://top.gg/bot/1271203231888052354#reviews")
     embed.timestamp = datetime.datetime.now()
     embed.url = "https://discord.gg/sJuQPRkDkq"
     await channel.send(f"<t:{round(datetime.datetime.now().timestamp())}:T> or <t:{round(datetime.datetime.now().timestamp())}:R>",embed=embed)
